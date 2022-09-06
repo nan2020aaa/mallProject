7ce16c0b71386ea068351e6fb1ab5faf98426e0f
573505184 <ck573505184@icloud.com>
2022-09-06 12:05:28 +0900

    创建了Oms的model

---
+++ mallProject/src/main/java/com/example/demo/models/oms/OmsMoneyInfoParam.java
@@ -0,0 +1,33 @@
+package com.example.demo.models.oms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class OmsMoneyInfoParam {
+
+	//管理员后台调整订单所使用的折扣金额
+	@Column
+	private Number discountAmount;
+
+	//运费金额
+	@Column
+	private Number freightAmount;
+
+	//订单ID
+	@Id
+	private Long orderId;
+
+	//订单状态：0->待付款；1->待发货；2->已发货；3->已完成；4->已关闭；5->无效订单
+	@Column
+	private Integer status;
+
+}
