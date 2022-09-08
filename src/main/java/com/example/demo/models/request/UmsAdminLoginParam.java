f4642f122e319b8876ebb1ccebdadfb9e01368fe
573505184 <ck573505184@gmail.com>
2022-09-08 17:01:17 +0900

    LoginUtilを追加した

--- src/main/java/com/example/demo/models/request/UmsAdminLoginParam.java
+++ src/main/java/com/example/demo/models/request/UmsAdminLoginParam.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UmsAdminLoginParam {
 	//密码
 	private String password;
 	
 	//用户名
 	private String username;
+	
 }
