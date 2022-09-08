6ca3bf07ff54be2d88d9a0515f0ea92154575c49
2013liliy <ly13253211668@gmail.com>
2022-09-08 17:51:17 +0900

    PmsProductServiceの修正

--- src/main/java/com/example/demo/services/pms/PmsProductService.java
+++ src/main/java/com/example/demo/services/pms/PmsProductService.java
@@ -1,10 +1,12 @@
 package com.example.demo.services.pms;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.models.request.PmsProductParam;
 import com.example.demo.repositories.pms.PmsProductRepository;
 
+@Service
 public class PmsProductService {
 
 	@Autowired
