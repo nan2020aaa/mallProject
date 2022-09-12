6d8243a981ea0d1057ccd79e8cbaa2b10983a796
2013liliy <ly13253211668@gmail.com>
2022-09-12 17:28:47 +0900

    test

--- src/main/java/com/example/demo/controllers/pms/PmsProductController.java
+++ src/main/java/com/example/demo/controllers/pms/PmsProductController.java
@@ -52,21 +52,6 @@ public class PmsProductController {
 
 	@ResponseBody // 返回值为 ResponseBody 的内容
 	@GetMapping("/list")
-
-	public CommonResult list(@RequestParam Long brandId, @RequestParam String keyword, @RequestParam Integer pageNum,
-			@RequestParam Integer pageSize, @RequestParam Long productCategoryId, @RequestParam String productSn,
-			@RequestParam Integer publishStatus, @RequestParam Integer verifyStatus) {
-		PmsProduct pmsProduct = new PmsProduct();// 为了让程序不报错临时写的代码，功能实现后应删去。
-		List<Object> pmsProductList = new ArrayList<>();
-		pmsProductList.add(pmsProduct);// 为了让程序不报错临时写的代码，功能实现后应删去。
-		// pmsProductList.add(productService.getBybrandId());
-		// pmsProductList.add(productService.getBybrandKeyword());
-		// pmsProductList.add(productService.getBybrandProductCategoryId());
-		// pmsProductList.add(productService.getBybrandProductSn());
-		// pmsProductList.add(productService.getBybrandPublishStatus());
-		// pmsProductList.add(productService.getBybrandVerifyStatus());
-		return new CommonResult(200, new CommonPage(pmsProductList, pageNum, pageSize, 1l, 1), "OK");
-	}
 	public CommonResult list(
 			/* @RequestParam Long brandId, @RequestParam String keyword, */@RequestParam Integer pageNum,
 			@RequestParam Integer pageSize/*
@@ -76,7 +61,6 @@ public class PmsProductController {
 											 */) {
 
 //		List<Object> pmsProductList = new ArrayList<>();
-//
 //		pmsProductList.add(productService.getByBrandId(brandId));
 //		pmsProductList.add(productService.getByKeywords(keyword));
 //		pmsProductList.add(productService.getByProductCategoryId(productCategoryId));
