9e491778e0dbb910c9d25714b1b653150c0b9b92
yangzhennan <1010976078@gmail.com>
2022-09-13 11:06:37 +0900

    ブランドの作成と見せつけ機能の開発

---
+++ src/main/java/com/example/demo/controllers/pms/PmsBrandController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controllers.pms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.models.pms.PmsBrand;
+import com.example.demo.models.response.CommonResult;
+import com.example.demo.services.pms.PmsBrandService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("/brand") // 所有本类中方法的请求路径都以 /brand 开头
+@Controller
+@Slf4j
+public class PmsBrandController {
+	@Autowired
+	private PmsBrandService brandService;
+
+	@ResponseBody // 返回值为 ResponseBody 的内容
+	@PostMapping("/create")
+	public CommonResult create(@RequestBody PmsBrand param) { // 传入参数为 RequestBody （在文档中标识为 body）
+
+		log.info("PmsBrandController, /create, パラメータを受け取った：{}", param);
+
+		if (brandService.create(param)) {
+			log.info("DBに保存完成：{}", param);
+			return new CommonResult(200, null, "OK");
+		} else {
+			return new CommonResult(500, null, "System Error");
+		}
+	}
+
+	@ResponseBody // 返回值为 ResponseBody 的内容
+	@GetMapping("/listAll")
+	public CommonResult listAll() {
+		log.info("listAllメソッド導入成功。");
+		List<PmsBrand> targetList = new ArrayList<>();
+		brandService.findAll().forEach((e) -> {
+			targetList.add(e);
+		});
+		log.info("targetList添加成功、" + targetList.toString());
+		return new CommonResult(200, targetList, "OK");
+	}
+}
