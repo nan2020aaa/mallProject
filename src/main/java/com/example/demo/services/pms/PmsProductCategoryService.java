c168c8327d7e5b8aab8ea75d840d1d6705aaa6b8
2013liliy <ly13253211668@gmail.com>
2022-09-14 09:32:23 +0900

    PmsProductCategoryController

---
+++ src/main/java/com/example/demo/services/pms/PmsProductCategoryService.java
@@ -0,0 +1,27 @@
+package com.example.demo.services.pms;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.pms.PmsProductCategory;
+import com.example.demo.models.pms.PmsProductCategoryWithChildrenItem;
+
+@Service
+public class PmsProductCategoryService {
+	
+	
+	
+	
+	public List<PmsProductCategoryWithChildrenItem> findAll() {
+		return null;
+		
+		
+	}
+
+	public boolean create(PmsProductCategory children) {
+	
+		return false;
+	}
+
+}
