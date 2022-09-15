af1480c6016df5dae59b1c40daa47ee689591fdd
2013liliy <ly13253211668@gmail.com>
2022-09-15 11:38:45 +0900

    test

---
+++ src/main/java/com/example/demo/controllers/pms/PmsProductCategoryController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controllers.pms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.models.pms.PmsProductCategory;
+import com.example.demo.models.pms.PmsProductCategoryWithChildrenItem;
+import com.example.demo.models.request.PmsProductCategoryParam;
+import com.example.demo.models.request.PmsProductParam;
+import com.example.demo.models.response.CommonResult;
+import com.example.demo.services.pms.PmsProductCategoryService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("/productCategory") // 所有本类中方法的请求路径都以 /productCategory 开头
+@Controller
+@Slf4j
+public class PmsProductCategoryController {
+
+	@Autowired
+	private PmsProductCategoryService productCategoryService;
+
+	@ResponseBody // 返回值为 ResponseBody 的内容
+	@PostMapping("/create")
+	public CommonResult create(@RequestBody PmsProductCategory children) { // 传入参数为 RequestBody （在文档中标识为 body）
+
+		if (productCategoryService.create(children)) {
+
+			return new CommonResult(200, null, "OK");
+		} else {
+			return new CommonResult(500, null, "System Error");
+		}
+	}
+
+	@GetMapping("/list/withChildren")
+	public CommonResult listwithChildren() {
+		List<PmsProductCategoryWithChildrenItem> dataList = new ArrayList<>();
+		List<PmsProductCategory> getList = productCategoryService.findAll();
+
+		for (int i = 0; i < getList.size(); i++) {
+			PmsProductCategoryWithChildrenItem instance = new PmsProductCategoryWithChildrenItem();
+			dataList.add(instance);
+		}
+
+		// dataList.forEach(e)->{dataList};
+
+		return new CommonResult(200, dataList, "ok");
+
+	}
+
+}
--- src/main/java/com/example/demo/services/pms/PmsProductCategoryService.java
+++ src/main/java/com/example/demo/services/pms/PmsProductCategoryService.java
@@ -1,15 +1,33 @@
 package com.example.demo.services.pms;
 
+import java.util.List;
+
+import org.apache.logging.log4j.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.models.pms.PmsProductCategory;
+import com.example.demo.models.pms.PmsProductCategoryWithChildrenItem;
 import com.example.demo.repositories.pms.PmsProductCategoryRepository;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Service
-@Slf4j
 public class PmsProductCategoryService {
-	@Autowired
-	private PmsProductCategoryRepository repository;
+	
+	 @Autowired
+		private PmsProductCategoryRepository pmsProductCategoryRepository;
+	
+	public boolean create(PmsProductCategory data) {
+	
+		return true;
+	}
+	
+	public List<PmsProductCategory> findAll() {
+		
+		return pmsProductCategoryRepository.findAll();
+		
+		
+	}
+
+	
+
 }
